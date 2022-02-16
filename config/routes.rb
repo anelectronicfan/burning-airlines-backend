4c6377647f0f9c55773634175047894e2406b2bd
Yu Fan Zheng <anelectronicfan@Yus-MacBook-Pro.modem>
2022-02-16 23:34:05 +1100

    trying to pull

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
  
-resources :users
+  resources :users
 
-resources :reservations
+  resources :reservations
 
-resources :flights
-get '/flights' => 'flights#index'
+  resources :flights
+  get '/flights/:id/delete' => 'flights#destroy',as: 'destroy_flight'
 
-resources :airplanes
+  resources :airplanes
+
+  # AXIOS flight search form
+  get '/search' => 'flights#search'
 
 end
