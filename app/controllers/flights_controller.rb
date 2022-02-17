4dc15cb48d77909f85aabfb31fa7f76f10b9455b
Angela Zhang <a_zhang93@hotmail.com>
2022-02-17 11:58:14 +1100

    updated adminIndex in Flight-controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -51,7 +51,26 @@ class FlightsController < ApplicationController
     end
 
     def adminIndex
-        flights =  Flight.all.reverse
+        flights = Flight.all.reverse.map {|flight|
+        
+        updated_flight = flight.attributes #this makes a hash copy of the Flight so we can add to it
+        
+        #grab the reservations and plane for the flight
+        reservations = Reservation.where(flight_id: flight.id)
+        plane = Airplane.find_by(id: flight.airplane_id)
+        #calculate the remaining seats
+        total_seats = plane.total_rows * plane.total_columns
+        remaining_seats = total_seats - reservations.length
+
+        #add the remaining seats and the name of the plane to the hash
+        updated_flight[:remaining_seats] = remaining_seats
+        updated_flight[:plane_name] = plane.name
+        
+        #return the hash to add into the flights array
+        updated_flight
+        }
+
+
         render json: flights
     end
 
